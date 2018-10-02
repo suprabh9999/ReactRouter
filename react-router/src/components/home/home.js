d532b39afa78dbc0563494ca55fa1aa2c0e17ef4
Mona Lisa <official.suprabh@gmail.com>
2018-10-02 22:14:08 +0530

    Updates

--- react-router/src/components/home/home.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-const home =()=>{
-    return (
-        <div className='alignContainer'>
-            <h2> We are the innovators. </h2>
-        </div>
-    );
-}
-export default home;
--- react-router/src/components/pageNotFound.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-const pageNotFound = ()=>{
-    return(
-        <h3 style={{marginLeft:'50px'}}>"404" Page not found.</h3>
-    );
-}
-export default pageNotFound;
